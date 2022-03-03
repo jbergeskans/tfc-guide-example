7753e401eca19fc8ec12acf0d198502f23b1f331
Jesper Bergeskans <jesperbergeskans@devoteam.com>
2022-03-03 14:10:51 +0100

    gix

--- api.tf
+++ api.tf
@@ -4,3 +4,10 @@ resource "google_project_service" "project" {
 
   disable_dependent_services = true
 }
+
+resource "google_project_service" "project2" {
+  project = "jesper-tfcloud-testing"
+  service = "cloudresourcemanager.googleapis.com/"
+
+  disable_dependent_services = true
+}
